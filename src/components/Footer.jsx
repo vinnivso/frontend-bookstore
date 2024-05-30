35b3429fe7c68a4137705c3a618d9878b32484b6
kali-vinnivso <kali-vinnivso@kali-vinnivso.kali-vinnivso>
2024-05-30 17:52:37 -0300

    Develop: Footer and Header components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <div className="h-10 text-center font-semibold p-2 bg-transparent">
+      <span className="italic text-white">Created by </span>
+      <a href="https://github.com/LATerada" className="text-white hover:underline-offset-2">LATerada</a>
+    </div>
+  );
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <blockquote class="h-24 lg:h-20 lg:ml-20 pt-6 text-3xl md:text-4xl font-semibold italic tracking-wider text-center lg:text-start text-white">
+      MONTE A SUA
+      <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-gradient-to-tr from-pink-400 to-rose-500 relative inline-block">
+        <span class="relative text-white">LISTA DE LIVROS!</span>
+      </span>
+    </blockquote>
+  );
+};
